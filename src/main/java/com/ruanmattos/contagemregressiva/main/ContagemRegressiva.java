f9ec5db1869fd496f7eede398d12d458eb5b61cf
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-13 22:00:01 -0300

    Implementações realizadas e testadas

--- src/main/java/com/ruanmattos/contagemregressiva/main/ContagemRegressiva.java
+++ src/main/java/com/ruanmattos/contagemregressiva/main/ContagemRegressiva.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.ruanmattos.contagemregressiva.main;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,13 @@ package com.ruanmattos.contagemregressiva.main;
 public class ContagemRegressiva {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bem Vindo(a) ao software para realizar uma contagem regressiva");
+        System.out.print("Digite o número pelo qual a contagem regressiva irá começar: ");
+        Scanner s = new Scanner(System.in);
+        int contagem = s.nextInt();
+        System.out.println("Iniciando contagem regressiva:");
+        for (int i = contagem; i > 0; i--) {
+            System.out.println(i);
+        }
     }
 }
