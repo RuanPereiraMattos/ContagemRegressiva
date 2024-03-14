dceb160d574860264fcf9dcc7c073185ea8dfa39
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-13 21:41:40 -0300

    Primeira versão

---
+++ src/main/java/com/ruanmattos/contagemregressiva/main/ContagemRegressiva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ruanmattos.contagemregressiva.main;
+
+/**
+ *
+ * @author aluno
+ */
+public class ContagemRegressiva {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
